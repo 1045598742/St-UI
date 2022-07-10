6978d62bea2b6a7f150bcb80027d5db3019ef9c9
libo <1045598742@qq.com>
2022-07-11 00:04:42 +0800

    完善进度条组件

--- packages/components/progress/src/index.vue
+++ packages/components/progress/src/index.vue
@@ -1,11 +1,36 @@
 <template>
-	<div class="st-progress">
-		<div v-if="type === 'line'" class="st-progress__line">
+	<div :class="['st-progress', `st-progress__${type}`]" :style="progressStyle">
+		<div
+			v-if="type === 'line'"
+			class="st-progress__shell"
+			:style="{ height: `${strokeWidth}px` }"
+		>
 			<div
-				class="st-progress__value"
-				:style="{ width: `${modelValue}%` }"
-			></div>
+				class="st-progress__kernel"
+				:style="{ width: `${progressValue}%`, backgroundColor: getColor() }"
+			>
+				<slot name="text">
+					<span v-if="textInside" class="progress-inner__text">{{
+						format ? format(progressValue) : `${progressValue}%`
+					}}</span>
+				</slot>
+			</div>
 		</div>
+		<template v-else-if="type === 'circle'">
+			<svg
+				version="1.1"
+				xmlns="http://www.w3.org/2000/svg"
+				:viewBox="`0,0,${props.width},${props.width}`"
+			>
+				<path v-bind="getPathAttrs(true)" class="st-progress__shell"></path>
+				<path v-bind="getPathAttrs()" class="st-progress__kernel"></path>
+			</svg>
+			<slot name="text">
+				<div v-if="textInside" class="progress-inner__text">
+					{{ format ? format(progressValue) : `${progressValue}%` }}
+				</div>
+			</slot>
+		</template>
 	</div>
 </template>
 
@@ -15,16 +40,113 @@ export default {
 }
 </script>
 <script lang="ts" setup>
-import { PropType } from 'vue'
+import { typeUtils } from '@st-ui/utils/typeUtils'
+import { computed, PropType, CSSProperties, SVGAttributes } from 'vue'
+
+type PropColor = PropType<
+	| string
+	| { color: string; percentage: number }[]
+	| ((percentage: number) => string)
+>
 
 const props = defineProps({
-	modelValue: {
+	/** 进度数值 */
+	percentage: {
 		type: Number,
 		default: 0
 	},
+	strokeWidth: {
+		type: Number,
+		default: 20
+	},
+	/** 进度条的类型 */
 	type: {
 		type: String as PropType<'circle' | 'line'>,
 		default: 'line'
+	},
+	width: {
+		type: Number,
+		default: 120
+	},
+	color: {
+		type: [String, Function, Array] as PropColor,
+		default: ''
+	},
+	/** 是否展示进度条文字在进度条内部 */
+	textInside: {
+		type: Boolean,
+		default: false
+	},
+	/** svg的path两端的形状 */
+	strokeLinecap: {
+		type: String as PropType<'butt' | 'round' | 'square'>,
+		default: 'round'
+	},
+	format: {
+		type: [Function] as PropType<(...args: any[]) => string | any>,
+		default: undefined
 	}
 })
+
+const progressValue = computed(() => {
+	if (props.percentage <= 0) return 0
+	if (props.percentage >= 100) return 100
+	return props.percentage
+})
+
+const progressStyle = computed(() => {
+	const cssStyle: CSSProperties = {}
+	if (props.type === 'circle') {
+		cssStyle.width = `${props.width}px`
+		cssStyle.height = `${props.width}px`
+	}
+	return cssStyle
+})
+
+/**
+ * 圆环和仪表盘的path属性
+ * @param isShell 是否是躯壳
+ */
+function getPathAttrs(isShell?: boolean): SVGAttributes {
+	const strokeWidth = 6
+	const centerPoint = {
+		xAxis: props.width / 2,
+		yAxis: props.width / 2
+	} as const
+	const offsetAbs = centerPoint.xAxis - strokeWidth / 2
+	const pathAttributes: SVGAttributes = {
+		'stroke-width': strokeWidth,
+		'stroke-linecap': props.strokeLinecap,
+		fill: 'none',
+		d: `
+			M ${centerPoint.xAxis} ${centerPoint.yAxis}
+			m 0 -${offsetAbs}
+			a ${offsetAbs} ${offsetAbs} 0 1 1 0 ${offsetAbs * 2}
+			a ${offsetAbs} ${offsetAbs} 0 1 1 0 -${offsetAbs * 2}
+		`
+	}
+	if (!isShell) {
+		const perimeter = 2 * offsetAbs * Math.PI
+		pathAttributes.stroke = getColor()
+		pathAttributes['stroke-dasharray'] = `${
+			(perimeter * progressValue.value) / 100
+		} ${perimeter}`
+	}
+	return pathAttributes
+}
+
+/** 获取进度条的颜色 */
+function getColor() {
+	if (!props.color) {
+		return 'green'
+	}
+	if (typeUtils.isString(props.color)) {
+		return props.color
+	} else if (typeUtils.isFunction(props.color)) {
+		return props.color(progressValue.value)
+	} else if (typeUtils.isArray(props.color)) {
+		return props.color.find((item) => item.percentage >= progressValue.value)
+			?.color
+	}
+}
 </script>
--- src/views/progress.vue
+++ src/views/progress.vue
@@ -1,5 +1,31 @@
 <template>
-	<st-progress v-model="value"></st-progress>
+	<button @click="value += 10">+{{ value }}</button>
+	<button @click="value -= 10">-{{ value }}</button>
+	<st-progress
+		type="circle"
+		:percentage="value"
+		text-inside
+		:format="format"
+		:color="[
+			{ color: '#f56c6c', percentage: 20 },
+			{ color: '#e6a23c', percentage: 40 },
+			{ color: '#5cb87a', percentage: 60 },
+			{ color: '#1989fa', percentage: 80 },
+			{ color: '#6f7ad3', percentage: 100 }
+		]"
+	></st-progress>
+	<st-progress
+		type="line"
+		:percentage="value"
+		text-inside
+		:color="[
+			{ color: '#f56c6c', percentage: 20 },
+			{ color: '#e6a23c', percentage: 40 },
+			{ color: '#5cb87a', percentage: 60 },
+			{ color: '#1989fa', percentage: 80 },
+			{ color: '#6f7ad3', percentage: 100 }
+		]"
+	></st-progress>
 </template>
 
 <script lang="ts" setup>
@@ -7,8 +33,8 @@ import { ref } from 'vue'
 
 const value = ref(20)
 
-setTimeout(() => {
-	value.value = 40
-}, 3000)
+function format(percentage: number) {
+	return `${percentage}-进度`
+}
 </script>
 <style lang="scss"></style>
